075cd4e9ce211413c26a44002daf7468068c3ae8
Ade Fajr Ariav <ade.fajr.ariav@gmail.com>
2023-02-07 09:38:57 +0700

    Initial Commit

---
+++ LatihanDownloadTaskManagerUITests/LatihanDownloadTaskManagerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LatihanDownloadTaskManagerUITestsLaunchTests.swift
+//  LatihanDownloadTaskManagerUITests
+//
+//  Created by Ade Fajr Ariav on 07/02/23.
+//
+
+import XCTest
+
+final class LatihanDownloadTaskManagerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
